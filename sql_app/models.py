fd2878a694c8c3a9966b56f9045df5c85d70e271
Sneha <mohan@linux.com>
2022-03-02 18:19:02 -0500

    adding db models

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,1 +1,13 @@
-from sqlalchemy import Column, Boolean
+from sqlalchemy import Column, Boolean, Integer, String
+
+from .database import Base
+
+
+class Driver(Base):
+    __tablename__ = "drivers"
+    id = Column(Integer, primary_key=True, index=true)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    nationality = Column(String, nullable=False)
+    dob = Column(String, nullable=False)
+    url = Column()
